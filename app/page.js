b55888d423a6637107066c0b8536139c5a71087d
emrecoban <fbdarktole@gmail.com>
2023-09-16 19:19:39 +0300

    Init commit.

--- app/page.js
+++ app/page.js
@@ -1,113 +1,134 @@
-import Image from 'next/image'
+import { Separator } from "@/components/ui/separator";
+import { Badge } from "@/components/ui/badge";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@/components/ui/label";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+import tweets from "./tweets";
+//console.log("gelen veri => ", tweets[0].tweet.id, tweets[0].tweet.full_text)
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="mx-auto max-w-7xl border border-slate-200 rounded-lg m-6 p-6">
+      <div className="grid grid-cols-4">
+        <div className="col-span-3">
+          <Tabs defaultValue="tweets" className="w-full pr-6">
+            <TabsList>
+              <TabsTrigger value="tweets">Tweet List</TabsTrigger>
+              <TabsTrigger value="center">Delete Center</TabsTrigger>
+            </TabsList>
+            <TabsContent value="tweets">
+              <div className="grid w-full gap-1.5">
+                <Label htmlFor="message-2">tweets.js</Label>
+                <Textarea placeholder="Paste your tweets.js" id="message-2" />
+                <p className="text-sm text-muted-foreground">
+                  Paste your tweets.js array to the textbox.
+                </p>
+                <Accordion
+                  type="single"
+                  collapsible
+                  className="w-full text-sm mt-6"
+                >
+                  <AccordionItem value="item-1">
+                    <AccordionTrigger>What is the tweets.js?</AccordionTrigger>
+                    <AccordionContent>
+                      This JSON file contains available Tweets which have not
+                      been deleted and it includes edited tweets if applicable.
+                      Users can edit a tweet up to five times; as such there are
+                      up to 5 edited tweets with unique “editTweetIds,” all
+                      connected by the “initialTweetID.” The definitions for
+                      each of the variables that may be included in any
+                      particular Tweet are available in our API documentation:
+                      https://developer.twitter.com/en/docs/tweets/post-and-engage/api-reference/post-statuses-update.
+                    </AccordionContent>
+                  </AccordionItem>
+                  <AccordionItem value="item-2">
+                    <AccordionTrigger>
+                      How can I download my Twitter Data?
+                    </AccordionTrigger>
+                    <AccordionContent>
+                      <ol className="list-inside list-decimal">
+                        <li>Login your Twitter account</li>
+                        <li>
+                          Settings and Support &gt; Settings and privacy &gt;
+                          Your account &gt; Download an archive of your data
+                        </li>
+                      </ol>
+                    </AccordionContent>
+                  </AccordionItem>
+                  <AccordionItem value="item-3">
+                    <AccordionTrigger>
+                      How can I find the tweets.js file?
+                    </AccordionTrigger>
+                    <AccordionContent>
+                      <ol className="list-inside list-decimal">
+                        <li>Download your Twitter Data</li>
+                        <li>Extract the zip file</li>
+                        <li>Open the folder</li>
+                        <li>/data/tweets.js</li>
+                      </ol>
+                    </AccordionContent>
+                  </AccordionItem>
+                </Accordion>
+              </div>
+            </TabsContent>
+            <TabsContent value="center">Change your password here.</TabsContent>
+          </Tabs>
+        </div>
+        <div className="bg-slate-50 rounded-lg p-6 h-min">
+          <div className="space-y-1">
+            <h4 className="text-base font-medium leading-none">
+              Tweet Deleter <Badge>v0.0.1-beta</Badge>
+            </h4>
+            <p className="text-sm text-muted-foreground">
+              Free and easily delete your tweets.
+            </p>
+          </div>
+          <Separator className="my-4" />
+          <div className="flex h-5 items-center space-x-4 text-sm">
+            <div>
+              <Badge variant="outline">{tweets.length}</Badge> Tweets
+            </div>
+            <Separator orientation="vertical" />
+            <div>
+              <Badge variant="destructive">2306</Badge> Deleted
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
+
+/*
+  {
+    tweet: {
+      edit_info: [Object],
+      retweeted: false,
+      source: '<a href="http://twitter.com/download/android" rel="nofollow">Twitter for Android</a>',
+      entities: [Object],
+      display_text_range: [Array],
+      favorite_count: '0',
+      in_reply_to_status_id_str: '1414718446445727745',
+      id_str: '1414719326108721156',
+      in_reply_to_user_id: '425109467',
+      truncated: false,
+      retweet_count: '0',
+      id: '1414719326108721156',
+      in_reply_to_status_id: '1414718446445727745',
+      created_at: 'Mon Jul 12 22:52:26 +0000 2021',
+      favorited: false,
+      full_text: "Arada ilgimi çeken de yok değil. Yine de Twitter'ın bu tarafı bana göre değil. Twitter bu anlamıyla güzel hee. İstiyorsun haber sitesi oluyor; istiyorsun video sitesi oluyor; istiyorsun kitap alıntıları sitesi...",
+      lang: 'tr',
+      in_reply_to_screen_name: 'emreshepherd',
+      in_reply_to_user_id_str: '425109467'
+    }
+  },
+  
+*/
